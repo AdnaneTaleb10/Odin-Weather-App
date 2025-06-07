be09d90654f689974ae41a99ee546196c8921013
AdnaneTaleb10 <adnane102464@gmail.com>
2025-06-07 23:21:16 +0100

    uild current weather section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-import './style.css'
+import "./style.css";
 import setFavicon from "./modules/logo";
+import cloudSun from "./assets/icons/weather-icons/cloud-sun.svg";
+import windSoak from "./assets/icons/highlight-icons/windsock.svg";
+import uv from "./assets/icons/highlight-icons/white-balance-sunny.svg";
+import humidity from "./assets/icons/highlight-icons/water-outline.svg"
+import visibility from "./assets/icons/highlight-icons/eye-outline.svg"
+import thermometer from "./assets/icons/highlight-icons/thermometer.svg"
+
+const weatherIcon = document.querySelector("#weather-icon");
+weatherIcon.src = cloudSun;
+
+const windSoakIcon = document.querySelector("#windsock-icon");
+windSoakIcon.src = windSoak;
+
+const uvIcon = document.querySelector("#uv-icon");
+uvIcon.src = uv;
+
+const humidityIcon = document.querySelector("#humidity-icon");
+humidityIcon.src = humidity;
+
+const visibilityIcon = document.querySelector("#visibility-icon");
+visibilityIcon.src = visibility;
+
+const thermometerIcon = document.querySelector("#thermometer-icon");
+thermometerIcon.src = thermometer;
+
