3b49ee80811c9c13c55e5570e9a8bc2b33e89b25
AdnaneTaleb10 <adnane102464@gmail.com>
2025-06-18 18:47:47 +0100

    fetch weather data and render weekly weather cards

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import "./style.css";
 import setFavicon from "./modules/logo";
 import toggleSearchBar from "./modules/search";
 import showInfo from "./modules/InfoPopup";
+import fetchWeatherData from "./modules/weatherService";
+import displayWeather from "./modules/renderWeather";
+
+import { icons } from "./modules/icons";
+
 import cloudSun from "./assets/icons/weather-icons/cloud-sun.svg";
 import windSoak from "./assets/icons/highlight-icons/windsock.svg";
 import uv from "./assets/icons/highlight-icons/white-balance-sunny.svg";
-import humidity from "./assets/icons/highlight-icons/water-outline.svg"
-import visibility from "./assets/icons/highlight-icons/eye-outline.svg"
-import thermometer from "./assets/icons/highlight-icons/thermometer.svg"
+import humidity from "./assets/icons/highlight-icons/water-outline.svg";
+import visibility from "./assets/icons/highlight-icons/eye-outline.svg";
+import thermometer from "./assets/icons/highlight-icons/thermometer.svg";
+import displayWeekCards from "./modules/renderCards";
+
+const displayModeButton = document.querySelector("#switch-button");
+const weaklyButton = document.querySelector("#weeks-mode");
+const hourButton = document.querySelector("#hours-mode");
 
 const weatherIcon = document.querySelector("#weather-icon");
 weatherIcon.src = cloudSun;
@@ -29,8 +39,37 @@ thermometerIcon.src = thermometer;
 
 const weatherIconCards = document.querySelectorAll(".weather-icon-card");
 weatherIconCards.forEach((icon) => {
-    icon.src = cloudSun
-})
+  icon.src = cloudSun;
+});
 
 toggleSearchBar();
 showInfo();
+
+fetchWeatherData("Paris")
+  .then((data) => {
+    console.log("Weather Data:", data);
+    displayWeather({
+      location: data.resolvedAddress,
+      weatherDesc: data.currentConditions.conditions,
+      temp: data.currentConditions.temp + "°C",
+      date: `${new Date().toLocaleString("en-GB", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      })}`,
+      iconSrc: icons[data.currentConditions.icon],
+      windValue: data.currentConditions.windspeed + "km/h",
+      uvValue: data.currentConditions.uvindex,
+      humidityValue: data.currentConditions.humidity + "%",
+      visibilityValue: data.currentConditions.visibility,
+      feelsLikeValue: data.currentConditions.temp + "°C",
+    });
+
+    //displayWeekCards(data);
+  })
+  .catch((err) => {
+    console.error("❌ Error fetching weather:", err);
+  });
